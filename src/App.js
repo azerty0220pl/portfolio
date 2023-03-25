97308adcccc5d4d5e7f57489082fa8f333d67447
Szymon Kokot <azerty0220pl@gmail.com>
2023-03-25 20:33:21 +0100

    Home section made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './Azerty0220.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faFreeCodeCamp, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   return (
-    <div>
-      <header className="fluid-container">
-        <nav className="navbar navbar-light bg-light navbar-expand-lg justify-content-between fixed-top">
+    <div className="fluid-container">
+      <header>
+        <nav className="navbar bg-primary navbar-expand-lg justify-content-between fixed-top">
           <a href="#home" className="navbar-brand">
             <img className="img-fluid mx-2" style={{ 'height': '2rem' }} src={logo} alt=""/>
             Szymon Kokot
@@ -16,6 +18,19 @@ function App() {
           </ul>
         </nav>
       </header>
+      <main>
+        <section id="home" className="vh-100 bg-secondary d-flex align-items-center justify-content-center">
+          <div>
+            <h1 className="text-center">Szymon Kokot</h1>
+            <p className="text-center">Fullstack web developer and mobile game developer.</p>
+            <div className="d-flex justify-content-evenly">
+              <a href="https://www.freecodecamp.org/SzymonKokot"><FontAwesomeIcon className="text-dark" icon={ faFreeCodeCamp } /></a>
+              <a href="https://github.com/azerty0220pl"><FontAwesomeIcon className="text-dark" icon={ faGithub } /></a>
+              <a href="https://www.linkedin.com/in/szymon-kokot-b3143b26a/"><FontAwesomeIcon className="text-dark" icon={ faLinkedin } /></a>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
