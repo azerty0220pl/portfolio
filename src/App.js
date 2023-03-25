23a8dd0f144b3a26f3d2a2b3e34562184e94799f
Szymon Kokot <azerty0220pl@gmail.com>
2023-03-25 19:05:42 +0100

    Header made

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import logo from './Azerty0220.png';
+
 function App() {
   return (
     <div>
-      
+      <header className="fluid-container">
+        <nav className="navbar navbar-light bg-light navbar-expand-lg justify-content-between fixed-top">
+          <a href="#home" className="navbar-brand">
+            <img className="img-fluid mx-2" style={{ 'height': '2rem' }} src={logo} alt=""/>
+            Szymon Kokot
+          </a>
+          <ul className="navbar-nav mx-2">
+            <li className="nav-item"><a className="nav-link" href="#about">About me</a></li>
+            <li className="nav-item"><a className="nav-link" href="#certifications">Certifications</a></li>
+            <li className="nav-item"><a className="nav-link" href="#games">Games</a></li>
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 }
