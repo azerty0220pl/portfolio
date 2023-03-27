ab1cfcc364799ffa4b13e2d3f5c35402def25fda
Szymon Kokot <azerty0220pl@gmail.com>
2023-03-27 10:14:59 +0200

    Games Section made

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './Azerty0220.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faFreeCodeCamp, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faFreeCodeCamp, faLinkedin, faGooglePlay } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   return (
@@ -53,7 +53,66 @@ function App() {
           <h3 className='text-center m-5'><a className="text-dark" href="https://www.freecodecamp.org/certification/SzymonKokot/back-end-development-and-apis" target="_blank">Back End Development and APIs Certification</a></h3>
         </section>
         <section id="games" className='bg-light p-1'>
-
+          <h1 className='text-center m-3'>Games</h1>
+          <p className="text-center">
+            Mobile games I made with Unity3D, c#, blender and inkscape. All of them are published 
+            on Play Store.
+          </p>
+          <div className='d-flex justify-content-evenly m-4'>
+            <img className="rounded" src="https://play-lh.googleusercontent.com/0rPcTliU7j7UNpHSwMGuYuI3K5LSINBbxw_TvFsuRrxZI3Ofo7jbRIhuA26CZhAO9Q=w240-h480-rw" alt='Square Run Icon' />
+            <div className='w-75 d-flex flex-column justify-content-center align-items-center'>
+              <h3 className='text-center'>Square Run</h3>
+              <p className='text-center'>Avoid obstacles, climb, find the entrance, climb, avoid obstacles, climb, climb, climb...</p>
+              <div className='w-50 d-flex justify-content-evenly'>
+                <a className="text-dark" target="_blank" href="https://github.com/azerty0220pl/VerticalFlappyCube"><FontAwesomeIcon className="text-dark" icon={ faGithub } /></a>
+                <a className="text-dark" target="_blank" href="https://play.google.com/store/apps/details?id=com.azerty0220pl.climbingsquare"><FontAwesomeIcon className="text-dark" icon={ faGooglePlay } /></a>
+              </div>
+            </div>
+        </div>
+        <div className='d-flex justify-content-evenly m-4'>
+          <img className="rounded" src="https://play-lh.googleusercontent.com/YrJkd8h1WaQ6pJxpgYfP6_7R5ZKG4Ke8cBBtIoc0qYGVq4pb1rsGWLLqWWdjhBhafyc=w240-h480-rw" alt='Neon Ride II Icon' />
+          <div className='w-75 d-flex flex-column justify-content-center align-items-center'>
+            <h3 className='text-center'>Neon Ride II</h3>
+            <p className='text-center'>Second part of Neon Ride, will you be able to go even further?</p>
+            <div className='w-50 d-flex justify-content-evenly'>
+              <a className="text-dark" target="_blank" href="https://github.com/azerty0220pl/Neon_Ride_II"><FontAwesomeIcon className="text-dark" icon={ faGithub } /></a>
+              <a className="text-dark" target="_blank" href="https://play.google.com/store/apps/details?id=com.azerty0220pl.neonrideii"><FontAwesomeIcon className="text-dark" icon={ faGooglePlay } /></a>
+            </div>
+          </div>
+        </div>
+        <div className='d-flex justify-content-evenly m-4'>
+          <img className="rounded" src="https://play-lh.googleusercontent.com/TrMoCrluSaEtwFE7aHLKP51RBYVrqrSqb5PZWi2l1Kp8V3n0-1y4bSrmMQLJYBp-3r8=w240-h480-rw" alt='Neon Ride Icon' />
+          <div className='w-75 d-flex flex-column justify-content-center align-items-center'>
+            <h3 className='text-center'>Neon Ride</h3>
+            <p className='text-center'>Go as fast and as far as you can. Will you be able to avoid all obstacles in your way?</p>
+            <div className='w-50 d-flex justify-content-evenly'>
+              <a className="text-dark" target="_blank" href="https://github.com/azerty0220pl/Neon_Ride"><FontAwesomeIcon className="text-dark" icon={ faGithub } /></a>
+              <a className="text-dark" target="_blank" href="https://play.google.com/store/apps/details?id=com.Azerty0220pl.NeonRide"><FontAwesomeIcon className="text-dark" icon={ faGooglePlay } /></a>
+            </div>
+          </div>
+        </div>
+        <div className='d-flex justify-content-evenly m-4'>
+          <img className="rounded" src="https://play-lh.googleusercontent.com/6OTDWhgWwbyQyDJmiHPadXHrk3gw7nn9hUurNQCtEvWjWeVT4NBKY_nZIAApOMiH-Q=w300" alt='Flappy Cube 2D Icon' />
+          <div className='w-75 d-flex flex-column justify-content-center align-items-center'>
+            <h3 className='text-center'>Flappy Cube 2D</h3>
+            <p className='text-center'>Try to get as high as you can without touching the black walls on both sides! Tap the screen and try to get to the end of the journey.</p>
+            <div className='w-50 d-flex justify-content-evenly'>
+              <a className="text-dark" target="_blank" href="https://github.com/azerty0220pl/Flappy-Cube"><FontAwesomeIcon className="text-dark" icon={ faGithub } /></a>
+              <a className="text-dark" target="_blank" href="https://play.google.com/store/apps/details?id=com.Azerty0220pl.FlappyCube"><FontAwesomeIcon className="text-dark" icon={ faGooglePlay } /></a>
+            </div>
+          </div>
+        </div>
+        <div className='d-flex justify-content-evenly m-4'>
+          <img className="rounded" src="https://play-lh.googleusercontent.com/2Kdr0aqBE-YeHGfm9usYtF0ltoPPUsZji3SagvLNZzxeP8W2jlZXn_Yn52nfJmCCIHg=w240-h480-rw" alt='Cube Climb Icon' />
+          <div className='w-75 d-flex flex-column justify-content-center align-items-center'>
+            <h3 className='text-center'>Cube Climb</h3>
+            <p className='text-center'>Avoid obstacles, choose your favourite skin and enjoy this endless runner. Get as high as you can, are you ready for the challenge?</p>
+            <div className='w-50 d-flex justify-content-evenly'>
+              <a className="text-dark" target="_blank" href="https://github.com/azerty0220pl/Cube-Run"><FontAwesomeIcon className="text-dark" icon={ faGithub } /></a>
+              <a className="text-dark" target="_blank" href="https://play.google.com/store/apps/details?id=com.azerty0220pl.CubeRun"><FontAwesomeIcon className="text-dark" icon={ faGooglePlay } /></a>
+            </div>
+          </div>
+        </div>
         </section>
       </main>
     </div>
