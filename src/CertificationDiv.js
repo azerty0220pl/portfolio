f665adc6e55cdbd71b9931257132446929e9dfd2
Szymon Kokot <azerty0220pl@gmail.com>
2023-04-15 20:08:22 +0200

    CertificationDiv upgrade

--- src/CertificationDiv.js
+++ src/CertificationDiv.js
@@ -5,12 +5,12 @@ class CertificationDiv extends React.Component {
     render () {
         return (
             <div className=''>
-                <h3 className='text-center m-5'><Link link={this.props.certificationLink}>{this.props.certificationTitle}</Link></h3>
-                <Link link={this.props.project1Link} className='text-center'>{this.props.project1Title}</Link>
-                <Link link={this.props.project2Link} className='text-center'>{this.props.project2Title}</Link>
-                <Link link={this.props.project3Link} className='text-center'>{this.props.project3Title}</Link>
-                <Link link={this.props.project4Link} className='text-center'>{this.props.project4Title}</Link>
-                <Link link={this.props.project5Link} className='text-center'>{this.props.project5Title}</Link>
+                <h3 className='text-center mt-5'><Link link={this.props.certificationLink}>{this.props.certificationTitle}</Link></h3>
+                <p className='text-center'><Link link={this.props.project1Link} className='text-center'>{this.props.project1Title}</Link></p>
+                <p className='text-center'><Link link={this.props.project2Link} className='text-center'>{this.props.project2Title}</Link></p>
+                <p className='text-center'><Link link={this.props.project3Link} className='text-center'>{this.props.project3Title}</Link></p>
+                <p className='text-center'><Link link={this.props.project4Link} className='text-center'>{this.props.project4Title}</Link></p>
+                <p className='text-center'><Link link={this.props.project5Link} className='text-center'>{this.props.project5Title}</Link></p>
             </div>
         );
     }
