5af3cf1b19781273e2cd90046dcb141b7f96a818
Szymon Kokot <azerty0220pl@gmail.com>
2023-05-20 16:55:44 +0200

    games styled

--- src/Main/Components/GameDiv.js
+++ src/Main/Components/GameDiv.js
@@ -4,23 +4,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faGooglePlay } from '@fortawesome/free-brands-svg-icons';
 
 class GameDiv extends React.Component {
-    render () {
-        return (
-            <div className='row m-5'>
-              <div className="col-sm-4 d-flex align-items-center">
-                <img className='rounded img-fluid' src={this.props.icon} alt='' />
-              </div>
-              <div className='col-sm-8 d-flex flex-column justify-content-center align-items-center'>
-                <h3 className='text-center mt-3'>{this.props.gameName}</h3>
-                <p className='text-center'>{this.props.des}</p>
-                <div className='w-50 d-flex justify-content-evenly'>
-                  <Link link={this.props.git}><FontAwesomeIcon className="text-dark" icon={ faGithub } /></Link>
-                  <Link link={this.props.play}><FontAwesomeIcon className="text-dark" icon={ faGooglePlay } /></Link>
-                </div>
-              </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className='game'>
+        <img className='game-icon' src={this.props.icon} alt='' />
+        <div className='game-info'>
+          <h3 className='game-title'>{this.props.gameName}</h3>
+          <p className='sec-paragraph'>{this.props.des}</p>
+          <div className='links'>
+            <Link link={this.props.git}><FontAwesomeIcon className="text-dark" icon={faGithub} /></Link>
+            <Link link={this.props.play}><FontAwesomeIcon className="text-dark" icon={faGooglePlay} /></Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default GameDiv;
