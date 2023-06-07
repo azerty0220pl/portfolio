2fbf55fe00fcc928e035d844c5cea467f6625e4d
Szymon Kokot <azerty0220pl@gmail.com>
2023-06-07 09:38:56 +0200

    extension changes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Main from './Main/Main';
 import Secondary from './Secondary/Secondary';
-import CubeClimb from './Secondary/games/CubeClimb';
-import FlappyCube from './Secondary/games/FlappyCube';
-import NeonRide from './Secondary/games/NeonRide';
-import NeonRideII from './Secondary/games/NeonRideII';
-import SquareRun from './Secondary/games/SquareRun';
 
 import "./index.css";
 
@@ -23,27 +18,7 @@ const router = createHashRouter([
   {
     path: "/projects/",
     element: <Secondary />
-  },
-  /*{
-    path: "cubeclimb",
-    element: <CubeClimb />
-  },
-  {
-    path: "flappycube2d",
-    element: <FlappyCube />
-  },
-  {
-    path: "neonride",
-    element: <NeonRide />
-  },
-  {
-    path: "neonrideii",
-    element: <NeonRideII />
-  },
-  {
-    path: "squarerun",
-    element: <SquareRun />
-  }*/
+  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
